636ae5c45f78be77dd587f10c57adee2c3c61239
not qwerty142 <43281665+chaos511@users.noreply.github.com>
2020-08-30 09:59:12 -0500

    Add files via upload

--- discordjsuser.js
+++ discordjsuser.js
@@ -1,8 +1,12 @@
 const fetch = require("node-fetch");
 const WebSocket = require("ws");
-
+const fs = require("fs");
+const FormData=require("form-data")
 class Client {
   conuctor() {}
+  printMsg() {
+    console.log("This is a message from the demo package");
+  }
   async login(email, password) {
     var jsonn;
     await fetch("https://discord.com/api/v6/auth/login", {
@@ -32,17 +36,44 @@ class Messages {
   constructor(token) {
     this.token = token;
   }
-
-  sendMessage(chatid, content, nonce) {
+async getUserProfile(userId){
+  var jsonn;
+  await fetch(discordURL + "users/"+userId+"/profile", {
+    headers: {
+      Authorization: this.token,
+      "content-type": "application/json",
+    },
+    method: "GET"
+  }).then((res) => res.json()) // expecting a json response
+  .then((json) => (jsonn = json));
+return jsonn;
+}
+  sendMessage(chatid, content,embed, nonce) {
     return fetch(discordURL + "channels/" + chatid + "/messages#", {
       headers: {
         Authorization: this.token,
         "content-type": "application/json",
       },
-      body: JSON.stringify({ content: content, nonce: nonce, tts: false }),
+      body: JSON.stringify({ content: content, nonce: nonce, tts: false ,embed:embed}),
       method: "POST",
     });
   }
+  sendMessageWithAttachment(chatid, attachment,content,embed,nonce) {
+
+    var formData = new FormData();
+    formData.append('file', fs.createReadStream(attachment));
+    if(content){
+      formData.append('content', content);
+    }
+    return fetch(discordURL + "channels/" + chatid + "/messages#", {
+      headers: {
+        Authorization: this.token,
+      },
+      body: formData,
+      method: "POST",
+    });
+  }
+
   sendMessageWithTSS(chatid, content, nonce) {
     return fetch(discordURL + "channels/" + chatid + "/messages#", {
       headers: {
@@ -80,22 +111,112 @@ class Messages {
       method: "DELETE",
     });
   }
-  editMessage(chatid, messageid, content) {
+  editMessage(chatid, messageid, content,embed) {
     return fetch(discordURL + "channels/" + chatid + "/messages/" + messageid, {
       headers: {
         Authorization: this.token,
         "content-type": "application/json",
       },
-      body: JSON.stringify({ content: content }),
+      body: JSON.stringify({ content: content,embed:embed }),
       method: "PATCH",
     });
   }
   getMessage(chatid) {
-    return getMessages(chatid, 1);
+    return this.getMessages(chatid, 1);
   }
+  getMessageById(chatid, msgid) {
+    return fetch(discordURL + "channels/" + chatid + "/messages/" + msgid, {
+      headers: {
+        Authorization: this.token,
+        "content-type": "application/json",
+      },
+      method: "GET",
+    });
+  }
+  async getMessagesJSON(chatid, limit, before, enableCache) {
+    var channelCache;
+    if (limit == -1) {
+      limit = Number.MAX_SAFE_INTEGER;
+      if (enableCache == true) {
+        try {
+          channelCache = JSON.parse(await fs.readFileSync("channelCache/"+chatid + ".json"));
+        } catch (ignore) {}
+      }
+    }
+    if (typeof enableCache == "object") {
+      channelCache = enableCache;
+    }
+    var messageArray;
+    if (limit > 100) {
+      messageArray = await this.getMessagesJSON(
+        chatid,
+        100,
+        before,
+        channelCache
+      );
+      if (channelCache) {
+        for (var x in channelCache) {
+          if (channelCache[x].id == messageArray[messageArray.length - 1].id) {
+            console.log("found in cache")
+            messageArray = messageArray.concat(channelCache.slice(parseInt(x) + 1));
+            fs.writeFileSync("channelCache/"+chatid + ".json", JSON.stringify(messageArray,null,2));
+            return messageArray;
+          }
+        }
+      }
+      console.log(messageArray.length+"  -  "+before+"  -  "+(Number.MAX_SAFE_INTEGER-limit))
+      if (limit - 100 > 0 && messageArray.length == 100) {
+        await new Promise((resolve) => {
+          setTimeout(resolve, 0);
+        });
+        var tempMessageArray = await this.getMessagesJSON(
+          chatid,
+          limit - 50,
+          messageArray[99].id,
+          channelCache
+        );
+        messageArray = messageArray.concat(tempMessageArray);
+        if (enableCache == true) {
+          fs.writeFileSync("channelCache/"+chatid + ".json", JSON.stringify(messageArray,null,2));
+        }
+      }
+      return messageArray;
+    } else {
+      var messages;
+      if (before) {
+        messages = await this.getMessagesBefore(chatid, limit, before);
+      } else {
+        messages = await this.getMessages(chatid, limit);
+      }
+      messages = await messages.json();
+      // if(enableCache==true){
+      //   fs.writeFileSync(chatid+".json",JSON.stringify(messages))
+      // }
+      return messages;
+    }
+  }
+
   getMessages(chatid, limit) {
     return fetch(
       discordURL + "channels/" + chatid + "/messages?limit=" + limit,
+      {
+        headers: {
+          Authorization: this.token,
+          "content-type": "application/json",
+        },
+        method: "GET",
+      }
+    );
+  }
+  getMessagesBefore(chatid, limit, before) {
+    return fetch(
+      discordURL +
+        "channels/" +
+        chatid +
+        "/messages?limit=" +
+        limit +
+        "&before=" +
+        before,
       {
         headers: {
           Authorization: this.token,
@@ -128,30 +249,69 @@ class Messages {
 class Gateway {
   constructor(token) {
     this.gatewayURL = "wss://gateway.discord.gg";
+    // this.gatewayURL = "ws://127.0.0.1:8083";
+
     this.token = token;
   }
   connect() {
     this.websocket = new WebSocket(this.gatewayURL);
     this.websocket.onopen = this.onOpen;
+    this.websocket.onerror = this.onError;
     this.websocket.onmessage = this.onMessage;
     this.websocket.token = this.token;
+
     this.websocket.MESSAGE_CREATE = this.MESSAGE_CREATE;
+    this.websocket.MESSAGE_DELETE = this.MESSAGE_DELETE;
+    this.websocket.GUILD_MEMBERS_CHUNK = this.GUILD_MEMBERS_CHUNK;
+    this.websocket.GUILD_MEMBER_LIST_UPDATE = this.GUILD_MEMBER_LIST_UPDATE;
+    this.websocket.PRESENCE_UPDATE = this.PRESENCE_UPDATE;
+    this.websocket.READY = this.READY;
 
     this.websocket.heartbeat = function (ws) {
       ws.send('{"op":1,"d":0}');
     };
   }
-  onMessage = function (data) {
+  onMessage(data) {
     try {
       data.data = JSON.parse(data.data);
     } catch (ignore) {}
-    // console.log(data.data);
     switch (data.data.op) {
       case 0:
+        // console.log(data.data.t)
         switch (data.data.t) {
           case "MESSAGE_CREATE":
-            this.MESSAGE_CREATE(data.data.d);
+            if (this.MESSAGE_CREATE) {
+              this.MESSAGE_CREATE(data.data.d);
+            }
             break;
+          case "MESSAGE_DELETE":
+            if (this.MESSAGE_DELETE) {
+              this.MESSAGE_DELETE(data.data.d);
+            }
+            break;
+          case "GUILD_MEMBERS_CHUNK":
+            if (this.GUILD_MEMBERS_CHUNK) {
+              this.GUILD_MEMBERS_CHUNK(data.data.d);
+            }
+            break;
+          case "GUILD_MEMBER_LIST_UPDATE":
+            if (this.GUILD_MEMBER_LIST_UPDATE) {
+              this.GUILD_MEMBER_LIST_UPDATE(data.data.d);
+            }
+            break;
+            case "READY":
+              if (this.READY) {
+                this.READY(data.data.d);
+              }
+              break;
+              case "PRESENCE_UPDATE":
+                if (this.PRESENCE_UPDATE) {
+                  this.PRESENCE_UPDATE(data.data.d);
+                }
+                break;
+            default:
+              // console.log(data.data)
+              break;
         }
         break;
       case 10:
@@ -175,7 +335,7 @@ class Gateway {
         );
         break;
     }
-  };
+  }
   async login(email, password) {}
 }
 module.exports = { Client: Client, Messages: Messages, Gateway: Gateway };
